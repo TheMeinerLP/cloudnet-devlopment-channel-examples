47f26ba26d9d9c00f22f7eea8c7919c860acb640
TheMeinerLP <p.glanz@madfix.me>
2020-07-06 10:24:09 +0200

    Improve example

--- src/main/java/me/madfix/examples/http/CreateXenForoUserWithPureJava.java
+++ src/main/java/me/madfix/examples/http/CreateXenForoUserWithPureJava.java
@@ -1,9 +1,16 @@
 package me.madfix.examples.http;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class CreateXenForoUserWithPureJava {
 
@@ -16,8 +23,11 @@ public class CreateXenForoUserWithPureJava {
             httpURLConnection.setRequestMethod("POST");
             httpURLConnection.addRequestProperty("content-type", "application/x-ww-form-urlencoded");
             httpURLConnection.addRequestProperty("key", key);
-            try (OutputStream outputStream = httpURLConnection.getOutputStream()) { // <--- Returns a response as a stream
+            try (InputStream inputStream = httpURLConnection.getInputStream()) { // <--- Returns a response as a stream
                 // Closes the stream automatically
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+                List<String> responseLines = bufferedReader.lines().collect(Collectors.toList());
+                responseLines.forEach(System.out::println);
             } catch (IOException e) {
                 e.printStackTrace();
             }
